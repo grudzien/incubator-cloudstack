069abf78d321827d2c16ae2fa53b24b448a78ef1
Prasanna Santhanam <tsp@apache.org>
2013-03-04 19:11:36 +0530

    marvin-factories: inherit from ABSTRACT_FACTORY
    This is done to remove dependency on an ORM.
    
    + Additional APIs from multiple ip per nic.
    
    Signed-off-by: Prasanna Santhanam <tsp@apache.org>

--- tools/marvin/marvin/integration/lib/factory/CloudStackBaseFactory.py
+++ tools/marvin/marvin/integration/lib/factory/CloudStackBaseFactory.py
@@ -5,9 +5,9 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-#
+# 
 #   http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -15,30 +15,11 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from marvin.integration.lib.base import Account
-import hashlib
-from marvin.integration.lib.factory.CloudStackBaseFactory import *
-from marvin.integration.lib.utils import random_gen
+import factory
 
-class AccountFactory(CloudStackBaseFactory):
+class CloudStackBaseFactory(factory.Factory):
+    ABSTRACT_FACTORY = True
 
-    FACTORY_FOR = Account.Account
-
-    accounttype = 0
-    email = factory.LazyAttribute(lambda e: '{0}.{1}@cloudstack.org'.format(e.firstname, e.lastname).lower())
-    firstname = 'fname-'+random_gen()
-    lastname = 'lname-'+random_gen()
-    username = None
-
-    # Password Encoding
-    mdf = hashlib.md5()
-    mdf.update('password')
-    password = mdf.hexdigest()
-
-class AdminAccountFactory(AccountFactory):
-    accounttype = 1
-
-
-class DomainAdminFactory(AccountFactory):
-    accounttype = 2
-    domainid = None
+    @classmethod
+    def _create(cls, target_class, *args, **kwargs):
+        return target_class(*args, **kwargs)
